a307e4945a2c2c507b7fb8c5487582f736e4d097
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-25 14:57:46 +0100

    Done

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,2 +1,4 @@
 // Example
-window.alert("Hello World!");
+var quote = window.prompt("Give me the quote");
+var author = window.prompt("and now, give me who is the author");
+window.alert(`${author} says, ${quote}`);
