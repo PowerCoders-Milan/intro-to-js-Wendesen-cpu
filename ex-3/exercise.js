c6e042e3ab1a797d9555e35d1c9cc0cb04e5566b
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-25 14:22:12 +0100

    Done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,16 @@
 // Example
-window.alert("Hello World!");
+var x = window.prompt("Insert the first number");
+var y = window.prompt("Insert the first number");
+
+if(x > y){
+    window.alert(`${x} is greater than ${y}`);
+ 
+}
+
+else if(x < y){
+    window.alert(`${y} is greater than ${x}`);
+}
+
+else{
+    window.alert("The two numbers are equal");
+}
