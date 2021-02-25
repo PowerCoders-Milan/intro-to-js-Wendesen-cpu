d128bf8bb9fd17e20fc94924b5cae946c8409d22
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-25 14:14:25 +0100

    Finished

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,2 +1,6 @@
 // Example
-window.alert("Hello World!");
+var birthYear = window.prompt("Insert the year you were born,please");
+var year = window.prompt("At what year do you want to know you age?");
+
+var currentAge = year - birthYear;
+window.alert("by the year " + year + ", you will be " + currentAge + " years old.")
