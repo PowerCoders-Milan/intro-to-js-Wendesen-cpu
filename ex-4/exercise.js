c17488a87c0a3798095d785f28e0577c702b8a2c
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-25 14:40:03 +0100

    Done

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,17 @@
 // Example
-window.alert("Hello World!");
+ var name = window.prompt("Who can I call you dear?");
+ var state = window.prompt(`Greetings ${name} How as your day?`);
+
+  if(state == "bad"){
+      window.alert("That's too bad, tomorrow it's a new day.");
+
+  }
+
+  else if ( state == "good"){
+      
+    window.alert("I'm happy for you champ");
+
+  }
+else {
+    window.alert("what do you mean? I couldn't get it");
+}
